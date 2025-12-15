36175d26ea6079a7ebc3b0449e08388ecfaa9ded
Vinícius Rezende <vprezende.work@gmail.com>
2025-12-15 11:31:27 -0300

    chore: replace full Vercel Analytics setup with inject()

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Analytics } from '@vercel/analytics/react'
+import { inject } from '@vercel/analytics';
+
+inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Analytics />
-  </StrictMode>,
+  </StrictMode>
 )
