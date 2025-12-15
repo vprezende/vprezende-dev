418667a5eb425afa28da8594dc577ecd649bbbb8
Vinícius Rezende <vprezende.work@gmail.com>
2025-12-15 11:59:54 -0300

    fix: remove changes made by Vercel agent

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { inject } from '@vercel/analytics';
-
-inject()
+import AnalyticsLoader from './analytics'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
+    <AnalyticsLoader />
+  </StrictMode>,
 )
